a5f1a4f07f60ec026af148968180a6e77ec02799
robles1999 <robles1999@yahoo.com>
2023-11-20 19:46:06 -0500

    todo list displaying todo items

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { engine } from 'express-handlebars';
 import handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+import { v4 as uuid } from 'uuid';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, "db.json");
 
 const app = express();
 const PORT = 3000;
@@ -12,16 +19,36 @@ const PORT = 3000;
  */
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', [`${dirname(fileURLToPath(import.meta.url))}/views`]);
+app.set('views', [`${__dirname}/views`]);
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(`${dirname(fileURLToPath(import.meta.url))}/public`));
+app.use(express.static(`${__dirname}/public`));
+const adapter = new JSONFile(file);
+const defaultData = {
+    todos: [],
+};
+const db = new Low(adapter, defaultData);
+await db.read();
+const todoInput = handlebars.compile(readFileSync(`${__dirname}/views/partials/todo-input.handlebars`, "utf-8"));
+const todoItem = handlebars.compile(readFileSync(`${__dirname}/views/partials/todo-item.handlebars`, "utf-8"));
 
 /**
  * Routes
  */
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const { todos } = db.data;
+    res.render('index', { partials: { todoInput, todoItem }, todos });
+});
+
+app.post('/todos', async (req, res) => {
+    const { todo } = req.body;
+    const newTodo = { id: uuid(), completed: false, name: todo };
+    db.data.todos.push(newTodo);
+    await db.write();
+    const { todos } = db.data;
+    res.render("index", { layout: false, partials: { todoInput, todoItem }, todos });
+    // setTimeout(() => {
+    // }, 2000);
 });
 
 app.listen(PORT, () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./views/**/*.handlebars"],
   theme: {
     extend: {
       fontFamily: {
